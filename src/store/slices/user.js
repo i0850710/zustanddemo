3f14ce1a5f2084cae9589f783669de64381ea225
小手 <i0850710@gmail.com>
2022-08-24 21:53:01 +0800

    remove slices

--- src/store/slices/user.js
+++
@@ -1,8 +0,0 @@
-
-export const createUserSlice = set => ({
-  user: null,
-  async fetchUser() {
-    const res = await fetch('https://api.publicapis.org/random')
-    set({ user: (await res.json()).entries[0] })
-  }
-})
